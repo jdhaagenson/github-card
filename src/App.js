f3b5121c68fe5f82075eb603378fe76c93d4bc2b
Jordan Haagenson <jdhaagenson@gmail.com>
2020-02-06 04:35:53 -0800

    forgot curlys

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ render() {
       <button onClick={this.handleToggle}>Toggle User</button>
       {this.state.active && (
         <>
-          <img src=this.state.user.avatar_url alt="user profile"></img>
+          <img src={this.state.user.avatar_url} alt="user profile"></img>
           <h1>{this.state.user.name}</h1>
+          <p>Location: {this.state.location}</p>
           <p>Bio: {this.state.user.bio}</p>
-          <p>Followers: {this.state.user.followers}</p>
         </>
       )}
     </>
