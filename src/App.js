ee7eded611311c74f7969b7ca0c738d3f9a92339
Jordan Haagenson <jdhaagenson@gmail.com>
2020-02-06 04:23:09 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component {
-  // this.setState
+const githubUserURL = githubUsername =>
+  `https://api.github.com/users/${githubUsername}`
 
-  handleToggle = (event) ={
-
+  class App extends Component {
+  state = {
+    user: {},
+    active: false
   }
+
+  handleToggle = (event) =>{
+    if (this.state.active===false){
+      fetch(githubUserURL('jdhaagenson'))
+        .then(response => response.json())
+        .then(responseBody => {
+          console.log(responseBody);
+          this.setState({user: responseBody, active:true})
+        });
+    } else {
+      this.setState({active: false})
+    }
+
+  };
+
 render() {
-  return (<button onClick={this.handleToggle}></button>)
+  return (
+    <>
+      <button onClick={this.handleToggle}>Toggle User</button>
+      {this.state.active && (
+        <>
+          <img src=this.state.user.avatar_url alt="user profile"></img>
+          <h1>{this.state.user.name}</h1>
+          <p>Bio: {this.state.user.bio}</p>
+          <p>Followers: {this.state.user.followers}</p>
+        </>
+      )}
+    </>
+  );
 
 }
 
